b66b253d6a737585c8e18638b519b539a87f20d0
Nyuykimac <macnyuykimac@gmail.com>
2020-10-26 13:21:34 +0100

    modified and added modifications to the myApp(displaying static files content and rendering html)

--- myApp.js
+++ myApp.js
@@ -1,4 +1,13 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
+app.get('/', function(request,response){
+     response.send("Hello Express");
+});
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+  });
+  app.use(express.static(__dirname + "/public"));
+  
+
 module.exports=app;
