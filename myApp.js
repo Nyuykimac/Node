81370472c8c527c3c34da2c1b61dacdd4a83bd9f
Nyuykimac <macnyuykimac@gmail.com>
2020-10-26 13:50:09 +0100

    Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 console.log("Hello World");
+app.use(function(req,res, next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+  });
 app.get('/', function(request,response){
      response.send("Hello Express");
 });
